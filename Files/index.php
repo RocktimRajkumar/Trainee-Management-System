5c3a50f4bc23ccb90fc79b3a84f97a224162520a
Rocktim <rocktim53@gmail.com>
2018-06-03 22:28:26 +0530

    manage schedule link added to index.php

--- Files/index.php
+++ Files/index.php
@@ -12,7 +12,8 @@
 	<div class="a1-bar a1-black" style="width:800px; margin:0 auto;">
    	  <div class="a1-bar-item a1-hover-blue"><a style="text-decoration:none;" href="index.php">Home</a></div>
       <div class="a1-bar-item a1-hover-blue"><a style="text-decoration:none;" href="addprogram.php">Add Program</a></div> 
-      <div class="a1-bar-item a1-hover-blue"><a style="text-decoration:none;" href="addtrainee.php">Add Trainee</a></div>  
+      <div class="a1-bar-item a1-hover-blue"><a style="text-decoration:none;" href="addtrainee.php">Add Trainee</a></div>
+          <div class="a1-bar-item a1-hover-blue"><a style="text-decoration:none;" href="manageschedule.php">Manage Schedule</a></div>  
           <div class="a1-bar-item a1-hover-blue"><a style="text-decoration:none;" href="logout.php">Logout</a></div>  
     </div>
 </div>
