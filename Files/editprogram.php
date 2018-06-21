077ba7d30790681b9c3f6bb09af503046093ee60
Rocktim <rocktim53@gmail.com>
2018-06-21 16:58:27 +0530

    program type select show hide done

--- Files/editprogram.php
+++ Files/editprogram.php
@@ -26,118 +26,58 @@ function myProgram1(){
 
     var x = document.getElementById("select");
     var i = x.selectedIndex;
-    var oncampus=document.getElementById("oncampus");
-    var offcampus=document.getElementById("offcampus");
-    var research=document.getElementById("research");
-    var pgdaem=document.getElementById("pgdaem");
+    var oncampus=document.getElementById("table-1");
+    var offcampus=document.getElementById("table-2");
+    var regional=document.getElementById("table-3");
     
     switch(x.options[i].value){
         
         case "1":oncampus.style.display="table-row";
                  offcampus.style.display="none";
-                 research.style.display="none";
-                 pgdaem.style.display="none";
+                 regional.style.display="none";
             break;
         case "2":oncampus.style.display="none";
                  offcampus.style.display="table-row";
-                 research.style.display="none";
-                 pgdaem.style.display="none";
+                 regional.style.display="none";
             break;
             
         case "3":
                 oncampus.style.display="none";
                  offcampus.style.display="none";
-                 research.style.display="table-row";
-                 pgdaem.style.display="none";
-            break;
-        
-        case "4":oncampus.style.display="none";
-                 offcampus.style.display="none";
-                 research.style.display="none";
-                 pgdaem.style.display="table-row";
+                 regional.style.display="table-row";
             break;
     }
 }
     
-    function myProgramType(str,type){
-    if (str == "0") {
-        document.getElementById("programtype").innerHTML = "";
-        return;
-    } else { 
-        if (window.XMLHttpRequest) {
-            // code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp = new XMLHttpRequest();
-        }
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("programtype").innerHTML=this.responseText;
-                
-            }
-        };
-        xmlhttp.open("GET","programdetail.php?q="+str+"&t="+type,true);
-        xmlhttp.send();
-    }
-    }
 </script>
+
+<style>
+    #table-2, #table-3{
+        display:none;
+    }    
+</style>
+
 </head>
 
 <body>
  <?php include('menu.php'); ?>
 <div class="banner"><img src="images/banner.jpg" style="width:100%" ></div>
+<table align="center">
+ <tr>
+           	   <td height="35">PROGRAM TYPE:</td>
+           	   <td height="35"><select  name="select" id="select">
 
+                <option value='1'>ONCAMPUS</option>;
+                <option value='2'>OFFCAMPUS</option>;
+                <option value='3'>REGIONAL WORKSHOP</option>;
+										
+                </select>&ensp;<input type="button" class="a1-btn a1-blue"  name="search" onclick="myProgram1();"  id="search" value="SEARCH"></td>
+				
+         </tr>
+<table>
 
 <table class="table table-bordered datatable" id="table-1" border=1 align="center" style="margin-top:10px; width:1020px">
 
-			<th>
-				<tr>
-					<th>S.No</th>
-					<th>PROGRAM TYPE</th>
-					<th>PROGRAM NAME:</th>
-					<th>DURATION FROM:</th>
-					<th>DURATION TO:</th>
-					<th>VENUE:</th>
-					<th>Action</th>
-				</tr>
-			</th>		
-				<tbody>
-					<?php
-
-
-					$query  = "select * from offcampus";
-					//echo $query;
-					$result = mysqli_query($link, $query);
-					$sno    = 1;
-
-					if (mysqli_affected_rows($link) != 0) {
-					    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-					        $msgid = $row['sn'];
-					        
-					        
-					        echo "<tr><td>" . $sno . "</td>";
-					        echo "<td> OFFCAMPUS </td>";
-					        echo "<td>" . $row['title'] . "</td>";
-							echo "<td>" . $row['durationfrom'] . "</td>";
-							echo "<td>" . $row['durationto'] . "</td>";
-							echo "<td>" . $row['venue'] . "</td>";
-					        
-					        $sno++;
-					        
-					        echo '<td><a href=editprogramdetails.php?id="'.$row['sn'].'"><input type="button" class="a1-btn a1-blue" id="boxxe" style="width:100%" value="Edit Program" ></a><form action="deleleteprogram.php" method="post" onSubmit="return ConfirmDelete();"><input type="hidden" name="name" value="' . $msgid .'"/><input type="submit" id="button1" value="Delete Program" class="a1-btn a1-orange"/></form></td></tr>';
-					        
-							$msgid = 0;
-					    }
-					    
-					}
-
-					?>																
-				</tbody>
-		</table>
-		
-		
-		
-		
-		<table class="table table-bordered datatable" id="table-1" border=1 align="center" style="margin-top:10px; width:1020px">
-
 		
 				<tr>
 					<th>S.No</th>
@@ -146,9 +86,9 @@ function myProgram1(){
 					<th>DURATION FROM:</th>
 					<th>DURATION TO:</th>
 					<th>VENUE:</th>
-					<th>Action</th>
+					<th>ACTION</th>
 				</tr>
-		
+				
 				<tbody>
 					<?php
 
@@ -184,7 +124,9 @@ function myProgram1(){
 		</table>
 		
 		
-		<table class="table table-bordered datatable" id="table-1" border=1 align="center" style="margin-top:10px; width:1020px">
+		
+		
+		<table class="table table-bordered datatable" id="table-2" border=1 align="center" style="margin-top:10px; width:1020px">
 
 		
 				<tr>
@@ -194,7 +136,55 @@ function myProgram1(){
 					<th>DURATION FROM:</th>
 					<th>DURATION TO:</th>
 					<th>VENUE:</th>
-					<th>Action</th>
+					<th>ACTION</th>
+				</tr>
+		
+				<tbody>
+					<?php
+
+
+					$query  = "select * from offcampus";
+					//echo $query;
+					$result = mysqli_query($link, $query);
+					$sno    = 1;
+
+					if (mysqli_affected_rows($link) != 0) {
+					    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+					        $msgid = $row['sn'];
+					        
+					        
+					        echo "<tr><td>" . $sno . "</td>";
+					        echo "<td> OFFCAMPUS </td>";
+					        echo "<td>" . $row['title'] . "</td>";
+							echo "<td>" . $row['durationfrom'] . "</td>";
+							echo "<td>" . $row['durationto'] . "</td>";
+							echo "<td>" . $row['venue'] . "</td>";
+					        
+					        $sno++;
+					        
+					        echo '<td><a href=editprogramdetails.php?id="'.$row['sn'].'"><input type="button" class="a1-btn a1-blue" id="boxxe" style="width:100%" value="Edit Program" ></a><form action="deleleteprogram.php" method="post" onSubmit="return ConfirmDelete();"><input type="hidden" name="name" value="' . $msgid .'"/><input type="submit" id="button1" value="Delete Program" class="a1-btn a1-orange"/></form></td></tr>';
+					        
+							$msgid = 0;
+					    }
+					    
+					}
+
+					?>																
+				</tbody>
+		</table>
+		
+		
+		<table class="table table-bordered datatable" id="table-3" border=1 align="center" style="margin-top:10px; width:1020px">
+
+		
+				<tr>
+					<th>S.No</th>
+					<th>PROGRAM TYPE</th>
+					<th>PROGRAM NAME:</th>
+					<th>DURATION FROM:</th>
+					<th>DURATION TO:</th>
+					<th>VENUE:</th>
+					<th>ACTION</th>
 				</tr>
 				
 				<tbody>
