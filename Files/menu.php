0a14603e6ffc58cdf398667099cdc4be510d1294
Rocktim <rocktim53@gmail.com>
2018-06-23 02:26:47 +0530

    menu bar modified

--- Files/menu.php
+++ Files/menu.php
@@ -1,52 +1,57 @@
-<div class="a1-container a1-black">
-<div id="mbmcpebul_wrapper" style="width:fit-content">
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Untitled</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="index/mbcsmbmcp.css" type="text/css" />
+</head>
+<body>
+
+
+<div id="mbmcpebul_wrapper" style="max-width: 1000px;">
   <ul id="mbmcpebul_table" class="mbmcpebul_menulist css_menu">
-  <li><div class="buttonbg gradient_button gradient58"><div class="icon_1"><a href="admin.php" class="button_1">Home</a></div></div></li>
-  <li><div class="buttonbg gradient_button gradient58" style="width: 145px;"><div class="arrow"><div class="icon_2"><a>Program</a></div></div></div>
+  <li><div class="icon_1 buttonbg" style="width: 93px;"><a href="admin.php" class="button_1">Home</a></div></li>
+  <li><div class="arrow buttonbg" style="width: 129px;"><div class="icon_2"><a>Program</a></div></div>
     <ul class="img_16">
-    <li class="gradient_menuitem gradient36"><a href="addprogram.php" class="with_img_16" title=""><img src="index_files/mbico_mbmcp_1.png" alt="" />Add Program</a></li>
-    <li class="gradient_menuitem gradient36"><a href="editprogram.php" class="with_img_16" title=""><img src="index_files/mbico_mbmcp_2.png" alt="" />Manage Program</a></li>
-    
+    <li><a href="addprogram.php" class="with_img_16" title=""><img src="index/mbico_mbmcp_1.png" alt="" />Add Program</a></li>
+    <li class="last_item"><a href="editprogram.php" class="with_img_16" title=""><img src="index/mbico_mbmcp_2.png" alt="" />Manage Program</a></li>
     </ul></li>
-    
-  <li><div class="buttonbg gradient_button gradient58" style="width: 135px;"><div class="arrow"><div class="icon_3"><a>Trainee</a></div></div></div>
+  <li><div class="arrow buttonbg" style="width: 119px;"><div class="icon_3"><a>Trainee</a></div></div>
     <ul class="img_16">
-    <li class="gradient_menuitem gradient36"><a href="addtrainee.php" class="with_img_16" title=""><img src="index_files/mbico_mbmcp_4.png" alt="" />Add Trainee</a></li>
-    <li class="gradient_menuitem gradient36"><a href="managetrainee.php" class="with_img_16" title=""><img src="index_files/mbico_mbmcp_5.png" alt="" />Manage Trainee</a></li>
-    
+    <li><a href="addtrainee.php" class="with_img_16" title=""><img src="index/mbico_mbmcp_3.png" alt="" />Add Trainee</a></li>
+    <li class="last_item"><a href="managetrainee.php" class="with_img_16" title=""><img src="index/mbico_mbmcp_4.png" alt="" />Manage Trainee</a></li>
     </ul></li>
-    
-  <li><div class="buttonbg gradient_button gradient58" style="width: 151px;"><div class="arrow"><div class="icon_4"><a>Schedule</a></div></div></div>
+  <li><div class="arrow buttonbg" style="width: 135px;"><div class="icon_4"><a>Schedule</a></div></div>
     <ul class="img_16">
-    <li class="gradient_menuitem gradient36"><a href="addSchedule.php" class="with_img_16" title=""><img src="index_files/mbico_mbmcp_7.png" alt="" />Add Schedule</a></li>
-    <li class="gradient_menuitem gradient36"><a href="editschedule.php
-    " class="with_img_16" title=""><img src="index_files/mbico_mbmcp_8.png" alt="" />Manage Schedule</a></li>
+    <li><a href="addSchedule.php" class="with_img_16" title=""><img src="index/mbico_mbmcp_5.png" alt="" />Add Schedule</a></li>
+    <li class="last_item"><a href="editschedule.php" class="with_img_16" title=""><img src="index/mbico_mbmcp_6.png" alt="" />Manage Schedule</a></li>
     </ul></li>
-    
-     <li><div class="buttonbg gradient_button gradient58" style="width: 155px;"><div class="arrow"><div class="icon_4"><a>PGDAEM</a></div></div></div>
+  <li><div class="arrow buttonbg" style="width: 151px;"><div class="icon_5"><a>PGDAEM</a></div></div>
     <ul class="img_16">
-    <li class="gradient_menuitem gradient36"><a href="pgdaem.php" class="with_img_16" title=""><img src="index_files/mbico_mbmcp_4.png" alt="" />Add Trainee</a></li>
-    <li class="gradient_menuitem gradient36"><a href="uploadmaterial.php" class="with_img_16" title=""><img src="index_files/mbico_mbmcp_5.png" alt="" />Upload Material</a></li>
-    <li class="gradient_menuitem gradient36 last_item"><a href="uploadpgdaem.php" class="with_img_16" title=""><img src="index_files/mbico_mbmcp_6.png" alt="" />Upload Result</a></li>
+    <li><a href="pgdaem.php" class="with_img_16" title=""><img src="index/mbico_mbmcp_7.png" alt="" />Add Trainee</a></li>
+    <li><a href="uploadmaterial.php" class="with_img_16" title=""><img src="index/mbico_mbmcp_8.png" alt="" />Upload Material</a></li>
+    <li class="last_item"><a href="uploadpgdaem.php" class="with_img_16" title=""><img src="index/mbico_mbmcp_9.png" alt="" />Upload Result</a></li>
     </ul></li>
-    
-   <li><div class="buttonbg gradient_button gradient58" style="width: 130px;"><div class="icon_5"><a href="research.php" class="button_5">Research</a></div></div></li>     
-     
-      <li><div class="buttonbg gradient_button gradient58" style="width: 130px;"><div class="arrow"><div  style="padding-right:20px;" class="icon_5"><a class="button_5">Report</a></div></div></div>
-  <ul class="img_16">
-    <li class="gradient_menuitem gradient36"><a href="yearwise.php" class="with_img_16" title=""><img src="index_files/mbico_mbmcp_4.png" alt="" />Year Wise</a></li>
-    <li class="gradient_menuitem gradient36"><a href="reportprogramwise.php" class="with_img_16" title=""><img src="index_files/mbico_mbmcp_5.png" alt="" />Program Wise</a></li>
-    <li class="gradient_menuitem gradient36 last_item"><a href="uploadpgdaem.php" class="with_img_16" title=""><img src="index_files/mbico_mbmcp_6.png" alt="" />Trainee Wise</a></li>
-    <li class="gradient_menuitem gradient36 last_item"><a href="uploadpgdaem.php" class="with_img_16" title=""><img src="index_files/mbico_mbmcp_6.png" alt="" />State Wise</a></li>
-    </ul>
-  
-  </li>
-  
-  <li><div class="buttonbg gradient_button gradient58" style="width: 119px;"><div class="icon_6"><a href="logout.php" class="button_6">Logout</a></div></div></li>
+  <li><div class="icon_6 buttonbg"><a href="research.php" class="button_6">RESEARCH</a></div></li>
+  <li><div class="arrow buttonbg" style="width: 115px;"><div class="icon_7"><a>Report</a></div></div>
+    <ul class="img_16">
+    <li><a href="yearwise.php" class="with_img_16" title=""><img src="index/mbico_mbmcp_10.png" alt="" />Year Wise</a></li>
+    <li><a href="reportprogramwise.php" class="with_img_16" title=""><img src="index/mbico_mbmcp_11.png" alt="" />Program Wise</a></li>
+    <li><a class="with_img_16" title=""><img src="index/mbico_mbmcp_12.png" alt="" />Trainee Wise</a></li>
+    <li><a class="with_img_16" title=""><img src="index/mbico_mbmcp_13.png" alt="" />State Wise</a></li>
+    <li class="last_item"><a class="with_img_16 with_arrow" title=""><img src="index/mbico_mbmcp_14.png" alt="" />View Uploaded</a>
+      <ul class="img_16">
+      <li><a class="with_img_16" title=""><img src="index/mbico_mbmcp_15.png" alt="" />View PGDAEM Result</a></li>
+      <li><a class="with_img_16" title=""><img src="index/mbico_mbmcp_16.png" alt="" />View PGDAEM Material</a></li>
+      <li class="last_item"><a class="with_img_16" title=""><img src="index/mbico_mbmcp_17.png" alt="" />View Research Report</a></li>
+      </ul></li>
+    </ul></li>
+  <li><div class="icon_8 buttonbg" style="width: 103px;"><a href="logout.php" class="button_8">Logout</a></div></li>
   </ul>
-</div>
 </div>
 <!-- Menus will work without this javascript file. It is used only for extra
      effects, improved usability, compatibility with very old web browsers
      and support for touch screen devices. -->
-<script type="text/javascript" src="index_files/mbjsmbmcp.js"></script>
+<script type="text/javascript" src="index/mbjsmbmcp.js"></script>
+</body>
+</html>
