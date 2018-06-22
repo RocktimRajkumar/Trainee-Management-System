e085595a1366c1e3675f5e06344d2176f0a50597
Rocktim <rocktim53@gmail.com>
2018-06-22 09:43:46 +0530

    menu edit schedule change to manage schedule and delete schedule added

--- Files/scheduleEditView.php
+++ Files/scheduleEditView.php
@@ -6,7 +6,7 @@ $scID=$_GET['sid'];
 $sql="select * from pschedule where schedule_ID='".$scID."' and pdays=".$day." order by sn";
 
 $result=mysqli_query($link,$sql);
-
+if (mysqli_affected_rows($link) != 0){
 echo "<tr>
     <th>Time</th>
     <th>Session/Topic</th>
@@ -20,8 +20,11 @@ while($row=mysqli_fetch_assoc($result)){
   echo "<td><input type='text' value='".$row['facilitator']."'></td></tr>";
 }
 
-echo "<tr border='0'><td colspan='2' align='right'><input type='button' value='Update' onclick='sumbit()'></td>";
-echo "<td colspan='2' align='left'><input type='reset' value='Cancel'><td></tr>";
-
+echo "<tr border='0'><td colspan='4' align='center'><input type='button' value='Update' onclick='sumbit()'>";
+echo "<input type='button' value='Delete' onclick='delsche()'>";
+echo "<input type='reset' value='Cancel'></td></tr>";
+}
+else
+    echo "<h3>No Schedule Found</h3>";
 
 ?>
